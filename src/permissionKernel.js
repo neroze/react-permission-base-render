8944ef81cd42d021c85f28b56d09cf40471d8da3
Jumper <neerooze@gmail.com>
2018-07-24 13:39:53 +0545

    add comments and remove logs

--- src/permissionKernel.js
+++ src/permissionKernel.js
@@ -1,17 +1,33 @@
-import roles from "./permissions";
+import allPermissions from "./permissions";
 import { AbilityBuilder, Ability } from "@casl/ability";
 import { pick, each, get, set, indexOf } from "lodash";
 import { createCanBoundTo } from "@casl/react";
 
+/**
+ * Public class
+ */
 export default class Permissions {
   constructor(_roles) {
     this.roles = _roles;
   }
-
+  /**
+   * Gets permission for the user based on the role passed
+   * @return Object auth: casl/ability object with permission defined, Can: Wrapper component to render component based on permission
+   */
   getPermission = () => {
-    // console.log('Getting permissions for', this.roles)
+    /**
+     * new Permission kernel instance
+     * @type Kernel
+     */
     const _perm = new Kernel(this.roles);
+    /**
+     * Lock all the permission to read only
+     */
     _perm.lockPermissions();
+    /**
+     * get role based user Permissions
+     * @type Object
+     */
     this.permissions = _perm.userPermissions();
     return {
       auth: this.permissions,
@@ -20,27 +36,45 @@ export default class Permissions {
   };
 }
 
+/**
+ * Private Permission kernel class
+ */
 class Kernel extends Permissions {
-  constructor(roles) {
+  constructor(_userRoles) {
     super();
-    this.roles = roles;
-    // console.log('roles class this', this)
+    /**
+     * set user role
+     * @type Array ie: ["SCRIBE_MANAGER", "PLAN_MANAGER"]
+     */
+    this.roles = _userRoles;
+    /**
+     * set all permissions from JSON file to object
+     * @type JSON
+     */
+    this.rolePermissions = allPermissions;
   }
+
+  /**
+   * Returns current user roles
+   * @return {[type]} [description]
+   */
   getRoles = () => {
     return this.roles;
   };
 
-  roleCan = roles;
-
+  /**
+   * Locks all the json based permissions to read only
+   * @return context
+   */
   lockPermissions = () => {
-    for (let role in this.roleCan) {
-      // console.log(roleCan[role])
-      Object.defineProperty(this.roleCan, role, {
+    for (let role in this.rolePermissions) {
+      // consolethis.rolePermissions[role])
+      Object.defineProperty(this.rolePermissions, role, {
         writable: false
       });
 
-      for (let permission in this.roleCan[role]) {
-        Object.defineProperty(this.roleCan[role], permission, {
+      for (let permission in this.rolePermissions[role]) {
+        Object.defineProperty(this.rolePermissions[role], permission, {
           writable: false
         });
       }
@@ -48,38 +82,30 @@ class Kernel extends Permissions {
     return this;
   };
 
+  /**
+   * returns user role based permissions and ability instance
+   * @return Object
+   */
   userPermissions() {
     const { rules, can, cannot } = AbilityBuilder.extract();
     let userPermissions = {};
     let allPermissions;
     const roles = this.getRoles();
-    // console.log(
-    //   "get(roles, 'SYSTEM_ADMIN')",
-    //   get(roles, "SYSTEM_ADMIN"),
-    //   roles
-    // );
+
+    // check if user role has system admin permission, if yes enable all permissions or else set permission based on user roles
     if (indexOf(roles, "SYSTEM_ADMIN") >= 0) {
-      // console.log("------------- system admin is here ------------------");
-      allPermissions = this.roleCan;
+      allPermissions = this.rolePermissions;
     } else {
-      allPermissions = pick(this.roleCan, this.getRoles());
+      allPermissions = pick(this.rolePermissions, this.getRoles());
     }
 
     each(allPermissions, roles => {
       each(roles, (permissions, module) => {
-        // console.log("++++", module, permissions);
         each(permissions, (permission, action) => {
-          // console.log(" -->", get(userPermissions, `[${module}][${action}]`));
           if (!get(userPermissions, `[${module}][${action}]`)) {
+            // set module based action permission
             set(userPermissions, `[${module}][${action}]`, permission);
-            // console.log(
-            //   "Setting action",
-            //   action,
-            //   "permission : ",
-            //   permission,
-            //   " to module",
-            //   module
-            // );
+            // set ability
             if (get(userPermissions, `[${module}][${action}]`)) {
               can(action, module);
             } else {
